238ef64f7ce1c918abea4acf0ba0c13cb0fade8f
unknown <jumpeena@hotmail.com>
2015-08-18 12:26:17 +0700

    Before Lunch

--- app/src/main/java/th/lifang/easytraffic/MainActivity.java
+++ app/src/main/java/th/lifang/easytraffic/MainActivity.java
@@ -6,23 +6,49 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity
-{
+public class MainActivity extends AppCompatActivity {
     //---Explicit variable--
     private ListView trafficListView;
 
-//method onCreate
+    //method onCreate
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
         //Initial Widget
         initialWidget();
+
     }
 
+    // CreateListView
+    private void createListView() {
+        String[] strTitle = new String[20];
+        strTitle[0] = "ห้ามเลี้ยวซ้าย";
+        strTitle[1] = "ห้ามเลี้ยวขวา";
+        strTitle[2] = "ตรงไป";
+        strTitle[3] = "เลี้ยวขวา";
+        strTitle[4] = "เลี้ยวซ้าย";
+        strTitle[5] = "ออก";
+        strTitle[6] = "เข้า";
+        strTitle[7] = "ออก";
+        strTitle[8] = "หยุด";
+        strTitle[9] = "จำกัดความสูง";
+        strTitle[10] = "ทางแยก";
+        strTitle[11] = "ห้ามกลับรถ";
+        strTitle[12] = "ห้ามจอด";
+        strTitle[13] = "มีรถสวนทาง";
+        strTitle[14] = "ห้ามแซง";
+        strTitle[15] = "เข้า";
+        strTitle[16] = "หยุดตรวจ";
+        strTitle[17] = "จำกัดความเร็ว";
+        strTitle[18] = "จำกัดความกว้าง 2.5 ม.";
+        strTitle[19] = "จำกัดความสูง 5 ม.";
+    }
+
+
     private void initialWidget() {
-        trafficListView = findViewById(R.id.listView);
+        trafficListView = (ListView) findViewById(R.id.listView);
     }
 
     //method onCreateOptionsMenu
@@ -32,7 +58,8 @@ public class MainActivity extends AppCompatActivity
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
-//method onOptionsItemSelected
+
+    //method onOptionsItemSelected
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
--- app/src/main/java/th/lifang/easytraffic/MyAdapter.java
+++ app/src/main/java/th/lifang/easytraffic/MyAdapter.java
@@ -5,6 +5,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 /**
  * Created by jumpeena on 8/18/2015.
@@ -15,9 +17,9 @@ public class MyAdapter extends BaseAdapter
     //Explicit
     private Context objContext;
     private int[] iconInts;
-    private string[] titleStrings;
+    private String[] titleStrings;
 
-    public MyAdapter(Context objContext, int[] iconInts, string[] titleStrings) {
+    public MyAdapter(Context objContext, int[] iconInts, String[] titleStrings) {
         this.objContext = objContext;
         this.iconInts = iconInts;
         this.titleStrings = titleStrings;
@@ -33,9 +35,16 @@ public class MyAdapter extends BaseAdapter
     public long getItemId(int i) {return 0;}
 
     @Override
-    public View getView(int i, View view, ViewGroup viewGroup)
-    {
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        LayoutInflater objLayoutInflater = (LayoutInflater) objContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view1 = objLayoutInflater.inflate(R.layout.listview_traffic, viewGroup, false);
+        //Show Image
+        ImageView iconImageView = (ImageView) view1.findViewById(R.id.imageView);
+        iconImageView.setImageResource(iconInts[i]);
 
-        return null;
+        //show Text
+        TextView titleTextView=(TextView) view1.findViewById(R.id.textView);
+        titleTextView.setText(titleStrings[i]);
+        return view1;
     }
 } //Main Class
